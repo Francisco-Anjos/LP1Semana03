49232f321886ffb39a17891dbf3f65868277cce6
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-17 10:30:49 +0000

    Exercise 1 changing a string completion

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a string");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Now give me a character");
+            string character = Console.ReadLine();
+
+            string newphrase = "";
+
+                
+            foreach (var t in phrase)
+            {
+                if (t.ToString() != character) 
+                {
+                    newphrase += t;
+                }
+            }
+            Console.WriteLine(newphrase);
+
+        }
+    }
+}
