d94339c82b82c65fd4560cf37a48f5e5602a6182
Francisco Anjos <fcariaanjos@gmail.com>
2023-03-17 10:56:38 +0000

    created the matrix and its contents

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -6,7 +6,27 @@ namespace ArraySums
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tell me how many horizontal lines");
+            int x = Console.ReadLine();
+
+            Console.WriteLine("Tell me how many Vertical lines");
+            int y = Console.ReadLine();
+
+            float [,] matriz =  new float[x,y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y ; j++)
+                {
+                    Console.WriteLine($"Youre in {1},{j} position");
+                    Console.WriteLine("Give me a number");
+                    
+                    matriz[i,j] = Console.ReadLine();
+                    
+
+                }
+            }
+
         }
     }
 }
